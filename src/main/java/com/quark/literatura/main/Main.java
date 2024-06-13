30fe8d0b48f673c17c0f647b51e0626a526bf33b
AncaQuark <ancaquark@gmail.com>
2024-06-12 22:38:07 -0500

    Correcion de errores

--- src/main/java/com/quark/literatura/main/Main.java
+++ src/main/java/com/quark/literatura/main/Main.java
@@ -30,9 +30,10 @@ public class Main {
                     1 - Libros Disponibles
                     2 - Buscar libros por titulo
                     3 - Buscar libros por autor
-                    4 - Listar libros buscados
-                    5 - Listar autores buscados
-                    6 - Listar autores vivos en determinado año
+                    4 - Mostrar libros teniendo en cuenta si el autor estaba vivo en un rango de tiempo
+                    5 - Listar libros buscados
+                    6 - Listar autores buscados
+                    7 - Listar autores vivos en determinado año
                                         
                     0 - Salir
                     """;
@@ -52,6 +53,7 @@ public class Main {
                     buscarLibrosPorAutor();
                     break;
                 case 4:
+                    librosRangoTiempo();
                     break;
                 case 5:
                     break;
@@ -93,15 +95,65 @@ public class Main {
             System.out.println("");
         }
 
-        System.out.println("Introduzca el id del libro que desea añadir a su listado");
-        int idLibroSeleccionado = entrada.nextInt();
-        libroPorID(idLibroSeleccionado);
+        var agregar = -1;
+        while (agregar != 1 && agregar != 2) {
+            var opciones = """
+                    
+                    ¿Desea agregar algun libro a su listado?
+                    
+                    1 - Si
+                    2 - No
+                    
+                    """;
+            System.out.println(opciones);
+            agregar = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (agregar) {
+                case 1:
+                    System.out.println("Introduzca el id del libro que desea añadir a su listado");
+                    int idLibroSeleccionado = entrada.nextInt();
+                    libroPorID(idLibroSeleccionado);
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
     }
 
     private void libroPorID(int idLibroSeleccionado) {
         var json = consumirAPI.obtenerDatos(URL + "?ids=" + idLibroSeleccionado);
         DatosBiblioteca datosBiblioteca = convierteDatos.obtenerDatos(json, DatosBiblioteca.class);
         Biblioteca biblioteca = new Biblioteca(datosBiblioteca);
+
+        for (Libros libro : biblioteca.getDatosLibros()) {
+            System.out.println("");
+            System.out.println("-------- LIBRO AGREGADO --------");
+            System.out.println("Id: " + libro.getId());
+            System.out.println("Titulo: " + libro.getTitulo());
+            System.out.println("Autor: " + libro.getAutores());
+            System.out.println("Idioma: " + libro.getIdiomas());
+            System.out.println("Número de descargas: " + libro.getCantidadDescargas());
+            System.out.println("--------------------------------");
+            System.out.println("");
+        }
+    }
+
+    private void buscarLibrosPorAutor() {
+    }
+
+    private void librosRangoTiempo(){
+        System.out.println("Introduzca el año inicial:");
+        String inicio = entrada.nextLine();
+        System.out.println("Introduzca el año final:");
+        String fin = entrada.nextLine();
+        var json = consumirAPI.obtenerDatos(URL + "?author_year_start=" + inicio + "&author_year_end=" + fin);
+        DatosBiblioteca datosBiblioteca = convierteDatos.obtenerDatos(json, DatosBiblioteca.class);
+        Biblioteca biblioteca = new Biblioteca(datosBiblioteca);
+        System.out.println(biblioteca);
 
         for (Libros libro : biblioteca.getDatosLibros()) {
             System.out.println("");
@@ -114,8 +166,32 @@ public class Main {
             System.out.println("-----------------------");
             System.out.println("");
         }
-    }
 
-    private void buscarLibrosPorAutor() {
+        var agregar = -1;
+        while (agregar != 1 && agregar != 2) {
+            var opciones = """
+                    
+                    ¿Desea agregar algun libro a su listado?
+                    
+                    1 - Si
+                    2 - No
+                    
+                    """;
+            System.out.println(opciones);
+            agregar = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (agregar) {
+                case 1:
+                    System.out.println("Introduzca el id del libro que desea añadir a su listado");
+                    int idLibroSeleccionado = entrada.nextInt();
+                    libroPorID(idLibroSeleccionado);
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
     }
 }
